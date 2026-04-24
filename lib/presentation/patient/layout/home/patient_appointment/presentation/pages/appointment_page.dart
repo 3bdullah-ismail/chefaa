abc4311aa971340fa9407a90915d0434c49d9589
Emo2003 <mdhta477@gmail.com>
2026-04-24 16:04:19 +0300

    + finish appointment design + still search in home not change like that in booking + handle logic of search in booking(edit filter code)

---
+++ lib/presentation/patient/layout/home/patient_appointment/presentation/pages/appointment_page.dart
@@ -0,0 +1,28 @@
+import 'package:chefaa/core/widget/inside_app_bar.dart';
+import 'package:chefaa/presentation/patient/layout/home/presentation/widgets/appointment_card.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../../../../core/resources/values_manager.dart';
+
+class AppointmentPage extends StatelessWidget {
+  const AppointmentPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:const  PreferredSize(
+        preferredSize:  Size.fromHeight(120),
+        child: InsideAppBar(title: "My Appointments"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: AppPadding.p20,vertical: AppPadding.p18),
+        child: ListView.separated(
+          shrinkWrap: true,
+          itemBuilder: (_, _) => const DoctorCard(),
+          separatorBuilder: (_, _) => const SizedBox(height: 20,),
+          itemCount: 3,
+        ),
+      ),
+    );
+  }
+}
