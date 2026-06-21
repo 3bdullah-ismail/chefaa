6a2e8c80328a255d711bfd3403b3d6173814e5d1
Emo2003 <mdhta477@gmail.com>
2026-06-21 23:59:59 +0300

    + finish profile , chatbot for pharmacy

--- lib/core/resources/assets_manager.dart
+++ lib/core/resources/assets_manager.dart
@@ -5,6 +5,7 @@ const String svgPath = "assets/svg_images";
 
 abstract class ImageAssets {
   static const String onboarding1 = "$imagePath/onboarding_1.png";
+  static const String chatbot = "$imagePath/bot.png";
   static const String onboarding2 = "$imagePath/onboarding_2.png";
   static const String onboarding3 = "$imagePath/onboarding_3.png";
   static const String hospitalist = "$imagePath/Hospitalist.png";
